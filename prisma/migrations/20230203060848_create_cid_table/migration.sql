2d8af97cb947cb86437cd4262b99dd0595cd59e9
jaejeonglee <ljj5256@gmail.com>
2023-02-03 15:23:28 +0900

    :tada: Basic setting

---
+++ prisma/migrations/20230203060848_create_cid_table/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE `Cid` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `cid` VARCHAR(191) NOT NULL,
+    `type` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
