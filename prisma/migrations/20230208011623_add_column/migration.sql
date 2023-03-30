492cb767493f371baa4c51263c8bd02957df7090
jaejeonglee <ljj5256@gmail.com>
2023-03-30 11:39:04 +0900

    git cache sync

--- prisma/migrations/20230208011623_add_column/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `fileid` to the `Cid` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `Cid` ADD COLUMN `fileid` BIGINT NOT NULL,
-    MODIFY `size` BIGINT NOT NULL;
