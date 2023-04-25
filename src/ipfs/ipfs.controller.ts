5e49c13c95961d5e4c9d21db0a528da5cdcd52f2
jaejeonglee <ljj5256@gmail.com>
2023-04-25 12:56:40 +0900

    :fire: Remove logic
    for ipfs upload

--- src/ipfs/ipfs.controller.ts
+++ src/ipfs/ipfs.controller.ts
@@ -1,18 +1,10 @@
 import { Controller, Get, Post, Body, Param, UseGuards } from '@nestjs/common';
 import { IpfsService } from './ipfs.service';
-import { FileUploadDto } from '../dto/fileUpload.dto';
 import { JwtAuthGuard } from '../guards/auth.guard';
 
 @Controller('ipfs')
 export class IpfsController {
   constructor(private readonly ipfsService: IpfsService) {}
-
-  @Post('upload')
-  @UseGuards(JwtAuthGuard)
-  async uploadFile(@Body() fileDto: FileUploadDto): Promise<object> {
-    const result = await this.ipfsService.createFile(fileDto);
-    return result;
-  }
 
   //   @Get('valid/:fileid')
   //   @UseGuards(JwtAuthGuard)
--- src/ipfs/ipfs.service.ts
+++ src/ipfs/ipfs.service.ts
@@ -1,66 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
-import { FileUploadDto } from '../dto/fileUpload.dto';
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
+// import { firstValueFrom } from 'rxjs';
 @Injectable()
 export class IpfsService {
   constructor(
     private prismaService: PrismaService,
     private readonly httpService: HttpService,
   ) {}
-
-  //file 등록
-  async createFile(fileUploadDto: FileUploadDto): Promise<object> {
-    const { cid, productId, platform } = fileUploadDto;
-
-    try {
-      const date: any = new Date();
-      const unixDate = date / 1000;
-      const overOfPoint = unixDate.toString().split('.')[0];
-      const underOfPoint = unixDate.toString().split('.')[1];
-      const convertedUnixDate =
-        underOfPoint.length == 3
-          ? overOfPoint + underOfPoint
-          : overOfPoint + underOfPoint + '0';
-
-      const regex = /[^1-9]/g;
-      const convertedCid = cid.replace(regex, '');
-      const fileid = convertedUnixDate + convertedCid;
-
-      const foundFileId = await this.prismaService.file.findUnique({
-        where: {
-          file_id: fileid,
-        },
-      });
-
-      if (foundFileId) {
-        return {
-          success: false,
-          message: '동일한 cid로 다시 시도해주세요.',
-          data: { cid: cid },
-        };
-      } else {
-        await this.prismaService.file.create({
-          data: {
-            download_url: `https://ipfs.gen.foundation/ipfs/${cid}`,
-            file_id: fileid,
-            product_id: productId,
-            uploaded_platform: platform,
-          },
-        });
-        return {
-          success: true,
-        };
-      }
-    } catch (error) {
-      console.log(error);
-      return {
-        success: false,
-        message: '예상치 못한 오류가 발생했습니다.',
-      };
-    }
-  }
 
   // //FileId 체크
   // async findFile(fileid: FileUploadDto['cid']): Promise<any> {
